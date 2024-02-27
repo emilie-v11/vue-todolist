a8ecc694ad42f2fe4f713b134e6b4f21f6ac38b2
Vukovic Emilie <emilievukovic79@gmail.com>
2024-02-27 17:21:58 +0100

    First cleaning up

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+</style>
