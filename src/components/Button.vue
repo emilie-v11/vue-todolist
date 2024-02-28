8367b088598560b876433b9d465997e53ebee025
Vukovic Emilie <emilievukovic79@gmail.com>
2024-02-28 12:49:22 +0100

    Add props on Button for type and add Style in <style> balise

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,34 @@
 <script setup>
-const { color, textBtn } = defineProps({
+const { color, textBtn, type } = defineProps({
   color: String,
-  textBtn: String
+  textBtn: String,
+  type: String,
 })
 const emit = defineEmits(['btn-click'])
 </script>
 
 <template>
-  <button class="btn" @click="emit('btn-click')" :style="{ backgroundColor: color }">
+  <button :type="type" class="btn" @click="emit('btn-click')" :style="{ backgroundColor: color }">
     {{ textBtn }}
   </button>
 </template>
 
-<style></style>
+<style scoped>
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,9 @@ const emit = defineEmits(['toggle-add-task'])
 <template>
   <header class="header">
     <h1>{{ title }}</h1>
-    <Button @btn-click="emit('toggle-add-task')" :color="showAddTask ? 'red' : 'green'" :textBtn="showAddTask ? 'Close Form' : 'Add Task'"></Button>
+    <Button @btn-click="emit('toggle-add-task')" :color="showAddTask ? 'red' : 'green'" :textBtn="showAddTask ? 'Close Form' : 'Add Task'"
+    :type="'button'"
+    class="btn-toogle-form"></Button>
   </header>
 </template>
 
