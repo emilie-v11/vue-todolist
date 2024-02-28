6386e951dbb9591a3512014f03b2d6a161f241d3
Vukovic Emilie <emilievukovic79@gmail.com>
2024-02-28 13:01:26 +0100

    Remove useless class on Button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,9 +11,12 @@ const emit = defineEmits(['toggle-add-task'])
 <template>
   <header class="header">
     <h1>{{ title }}</h1>
-    <Button @btn-click="emit('toggle-add-task')" :color="showAddTask ? 'red' : 'green'" :textBtn="showAddTask ? 'Close Form' : 'Add Task'"
-    :type="'button'"
-    class="btn-toogle-form"></Button>
+    <Button
+      @btn-click="emit('toggle-add-task')"
+      :color="showAddTask ? 'red' : 'green'"
+      :textBtn="showAddTask ? 'Close Form' : 'Add Task'"
+      :type="'button'"
+    ></Button>
   </header>
 </template>
 
